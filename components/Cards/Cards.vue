9379cf64efeaefc2482087238bd1d52a9a8785dd
sanchitagithub12 <sanchitagithub@gmail.com>
2021-09-04 18:40:39 +0530

    completed

---
+++ components/Cards/Cards.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-container>
+    <scroll-wrap target="blog-list">
+      <h3 class="display-2 text-center mb-4">Latest News</h3>
+      <p class="body-1 text-center mb-4">
+        Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
+        accumsan lacus vel facilisis.
+      </p>
+      <div id="blog-list" class="blog-wrap">
+        <v-row justify="center">
+          <v-col md="9" cols="12">
+            <v-row>
+              <v-col
+                v-for="(item, index) in blogData"
+                :key="index"
+                md="4"
+                cols="12"
+                class="px-4"
+              >
+                <v-card max-width="500">
+                  <v-img :src="item.img" class="white--text" height="200px" />
+                  <v-card-title class="title">
+                    {{ item.title }}
+                  </v-card-title>
+                  <v-card-text>
+                    <span class="text--primary">
+                      <span>{{ item.desc }}</span>
+                    </span>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn text color="black"> Read more </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </div>
+    </scroll-wrap>
+  </v-container>
+</template>
+
+<style scoped lang="scss">
+//@import './blog-style';
+</style>
+
+<script>
+//import BlogCard from '../Cards/BlogCard'
+//import ScrollWrap from '../ScrollWrap'
+
+export default {
+  components: {
+    //BlogCard,
+    //ScrollWrap,
+  },
+  data() {
+    return {
+      blogData: [
+        {
+          title: "How to Document Processes to Scale Your Agency",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          img: "https://genial-react.envytheme.com/images/agency-blog1.jpg",
+        },
+        {
+          title: "15 Employee Feedback Tools to Track Your Team",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          img: "https://genial-react.envytheme.com/images/agency-blog2.jpg",
+        },
+        {
+          title: "12 Tried-and-True Ways to Improve User",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          img: "https://genial-react.envytheme.com/images/agency-blog3.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.title {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
---
+++ components/Header/Header.vue
@@ -0,0 +1,101 @@
+<!--template>
+  <div class="head">
+    <!-- :class="{ 'mobile': $vuetify.breakpoint.smAndDown, 'head': $vuetify.breakpoint.smAndUp}"  -->
+    <!--h1 class="mobile">
+      {{ title }}
+    </!--h1>
+    <p class="mobile">Let's Travel Together</p>
+  </div>
+</!--template>
+
+<!--script>
+export default {
+  data() {
+    return {
+      title: "Welcome",
+    };
+  },
+};
+</!--script>
+
+<!--style>
+.head {
+  text-align: center;
+}
+</!--style!-->
+
+<template>
+  <div>
+    <v-carousel cycle :height="$vuetify.breakpoint.smAndUp ? '600' : '600'">
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+        justify="center"
+      >
+        <h1 align="center" class="h1">
+          {{ item.title }}
+        </h1>
+        <h3 class="p">
+          {{ item.caption }}
+        </h3>
+        <v-btn class="btn" x-large rounded color="primary" dark align="center">
+          Get Started
+        </v-btn></v-carousel-item
+      >
+    </v-carousel>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvQOfYdk20E70vc5D1tV34WwC5jSSP3oEpLQ&usqp=CAU.jpg",
+          title: "Ultimate Platform to monitor your best workflow.",
+          caption:
+            "It should be noted that although application software is thought of asa program, it can be anything that runs on a computer.",
+        },
+        {
+          src: "https://image.shutterstock.com/image-photo/leaves-dark-shade-600w-1065614543.jpg",
+          title: "Ultimate Platform to monitor your best workflow.",
+          caption:
+            "It should be noted that although application software is thought of asa program, it can be anything that runs on a computer.",
+        },
+        {
+          src: "https://image.shutterstock.com/image-photo/leaves-dark-shade-600w-1065614543.jpg",
+          title: "Ultimate Platform to monitor your best workflow.",
+          caption:
+            "It should be noted that although application software is thought of asa program, it can be anything that runs on a computer.",
+        },
+        {
+          src: "https://wallpaperaccess.com/full/2957951.jpg",
+          title: "Ultimate Platform to monitor your best workflow.",
+          caption:
+            "It should be noted that although application software is thought of asa program, it can be anything that runs on a computer.",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.h1 {
+  color: white;
+  margin-top: 100px;
+}
+.p {
+  color: white;
+  font-size: 25px;
+  text-align: center;
+}
+.btn {
+  size: 50px;
+  align-content: center;
+  margin-left: 450px;
+  margin-top: 100px;
+}
+</style>
